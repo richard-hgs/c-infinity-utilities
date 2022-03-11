b63e0a632e8718df941526ea615e802e81b40fda
richard-hgs <35472546+richard-hgs@users.noreply.github.com>
2022-03-10 21:27:18 -0300

    InfinityCmake
    InfinityCmake

---
+++ InfinityCmake/InfinityPcre/InfinityPcre.h
@@ -0,0 +1,13 @@
+﻿// InfinityPcre.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+#include "H:/Programming/VSProjects/InfinityProjectsCpp/Pcre2-10.23-x86/pcre2_ucp.h"
+
+// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
+
+void testPcre() {
+	std::cout << "testando123" << std::endl;
+}
