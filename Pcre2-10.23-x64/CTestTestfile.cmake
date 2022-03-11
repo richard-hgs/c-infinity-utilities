e48bea19bfade4f0e39f16f9807a3c7a3fa1dc6c
richard-hgs <35472546+richard-hgs@users.noreply.github.com>
2022-03-10 21:42:47 -0300

    Pcre x64 upload
    Pcre x64 upload

---
+++ Pcre2-10.23-x64/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: H:/Programming/VSProjects/InfinityProjectsCpp/Pcre2-10.23-x86
+# Build directory: H:/Programming/VSProjects/InfinityProjectsCpp/Pcre2-10.23-x86
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(pcre2_test_bat "pcre2_test.bat")
+set_tests_properties(pcre2_test_bat PROPERTIES  PASS_REGULAR_EXPRESSION "RunTest\\.bat tests successfully completed" _BACKTRACE_TRIPLES "H:/Programming/VSProjects/InfinityProjectsCpp/Pcre2-10.23-x86/CMakeLists.txt;675;ADD_TEST;H:/Programming/VSProjects/InfinityProjectsCpp/Pcre2-10.23-x86/CMakeLists.txt;0;")
